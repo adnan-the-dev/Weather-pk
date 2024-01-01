0ee48a12984411b3c75c6df9122b180e09fef470
adnan <azizart320480@gmail.com>
2024-01-01 22:26:34 +0500

    some change

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -1,83 +1,77 @@
 import React, { useEffect, useState } from "react";
-import "./weather.css"
+import "./weather.css";
 
+const img =
+  "https://uxwing.com/wp-content/themes/uxwing/download/weather/weather-icon.png";
 
-const img = "https://uxwing.com/wp-content/themes/uxwing/download/weather/weather-icon.png"
-
-const url = "http://api.weatherapi.com/v1/current.json?key=cf7c3e48919d4d46b88203928231108&aqi=no&q="
+const url =
+  "http://api.weatherapi.com/v1/current.json?key=cf7c3e48919d4d46b88203928231108&aqi=no&q=";
 function Weather() {
+  const [search, setSearch] = useState("");
+  const [response, setReaponse] = useState({});
 
-    const [search, setSearch] = useState("")
-    const [response, setReaponse] = useState({})
-    const apiCall = () => {
-        fetch(url + search.replace(" ", "+"))
-            .then(res => res.json())
-            .then(data => setReaponse(data))
-    }
-    apiCall()
+  const apiCall = (e) => {
+    fetch(url + search.replace(" ", "+"))
+      .then((res) => res.json())
+      .then((data) => setReaponse(data));
+  };
 
-    return (
-        <>
+  return (
+    <>
+      <div className="box">
+        <div className="input">
+          <input
+            type="search"
+            className="inputFeild"
+            placeholder="Search Country"
+            onChange={(event) => {
+              setSearch(event.target.value);
+            }}
+          />
+          <button className="btn" onClick={() => apiCall()}>
+            Search
+          </button>
+        </div>
+        <div>
+          <div className="input">
+            <h1>Country:{response.location?.country || "Search"}</h1>
+          </div>
 
-            <div className="box">
-
-                <form className="input">
-                    <input type="search" className="inputFeild" placeholder="Search Country" onChange={(event) => { setSearch(event.target.value) }} />
-                </form>
-
-                {!search ? (
-                    <div style={{
-                        display: 'flex',
-                        justifyContent: 'center',
-                        flexDirection: 'column',
-                        alignItems: 'center',
-                        color: "white"
-                    }}>
-                        <h3 style={{
-                            fontSize: 'large',
-                            fontWeight: 'bolder'
-                        }}>No Data Found</h3>
-                        <span>Enter the country</span>
-                    </div>
-                ) : (
-
-
-                    <div>
-                        <div className="input"><h1>Country:{response.location?.country || "Search"}</h1></div>
-
-                        <div className="location">
-                            <img src="https://img.icons8.com/?size=512&id=k1fSzY8XQUE8&format=png" alt="" />
-                            <h2>City:{response.location?.name || ""}</h2>
-                        </div>
-                        <div className="temprature">
-
-                            <div className="cloud-icon">
-                                <img src={response.current?.condition.icon || img} alt="" />
-                            </div>
-                            <h2>{response.current?.temp_c || "0"}°C°F</h2>
-                        </div>
-
-                        <div className="tiem">
-                            <h3>Time:{response.location?.localtime || ""}</h3>
-                        </div>
-
-                        <div className="region"><h3>Region:{response.location?.region || ""}</h3></div>
-
-                        <div className="degree-section">
-                            <div className="degree-part">
-                                <span>Cloud:{response.current?.cloud || "0"}</span>
-                                <span>Wind_Degree:{response.current?.wind_degree || "0"}</span>
-                            </div>
-                            <div className="degree-part">
-                                <span>Longitude:{response.location?.lat || "0"}</span>
-                                <span>Latitude:{response.location?.lon || "0"}</span>
-                            </div>
-                        </div>
-                    </div>
-                )}
-
+          <div className="location">
+            <img
+              src="https://img.icons8.com/?size=512&id=k1fSzY8XQUE8&format=png"
+              alt=""
+            />
+            <h2>City:{response.location?.name || ""}</h2>
+          </div>
+          <div className="temprature">
+            <div className="cloud-icon">
+              <img src={response.current?.condition.icon || img} alt="" />
             </div>
-        </>
-    );
+            <h2>{response.current?.temp_c || "0"}°C°F</h2>
+          </div>
+
+          <div className="tiem">
+            <h3>Time:{response.location?.localtime || ""}</h3>
+          </div>
+
+          <div className="region">
+            <h3>Region:{response.location?.region || ""}</h3>
+          </div>
+
+          <div className="degree-section">
+            <div className="degree-part">
+              <span>Cloud:{response.current?.cloud || "0"}</span>
+              <span>Wind_Degree:{response.current?.wind_degree || "0"}</span>
+            </div>
+            <div className="degree-part">
+              <span>Longitude:{response.location?.lat || "0"}</span>
+              <span>Latitude:{response.location?.lon || "0"}</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
 export default Weather;
